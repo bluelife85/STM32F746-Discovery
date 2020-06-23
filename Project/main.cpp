3826f5e061a2084861fbfdfdf2a7a65b40253266
bluelife85 <bluelife85@naver.com>
2020-06-23 22:11:31 +0900

    전부 삭제. 이게 아니야

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,13 +1,7 @@
 #include "stm32f746.hpp"
-#include "stm32f746_system.hpp"
-
-#include "stm32f746_gpio.hpp"
 
 int main(void) {
     
-    GPIO GPIOI(GPIOIReg);
-    
-    GPIOI.setOutput(GPIO::PULL_STATE_NO, 17, 17);
     while(1) {
     }
 }
